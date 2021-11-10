81d7745c4300f3a29e5394069b9ae4581fa6aa63
Olivier Picaud <opicaud.mailbox@gmail.com>
2021-11-10 10:03:45 +0100

    extracting methods refactoring

--- contracts/Community.sol
+++ contracts/Community.sol
@@ -43,35 +43,37 @@ contract Community {
         newEvent.open = true;
     }
 
+
     function closeEvent() external {
         require(msg.sender == owner,"Only owner can stop an event");
         require(events[eventId].open == true, "An existing event must be active before stopping it");
         token.transferFrom(msg.sender, address(this), events[eventId].rewards);
         for (uint i=0;i<events[eventId].committedParticipants.length;i++){
-            address committedParticipants = events[eventId].committedParticipants[i];
-            if( members[committedParticipants].eventsToCommit == 0) {
-                uint totalReward = members[committedParticipants].currentEventRewards + members[committedParticipants].lastEventRewards;
-                token.transfer(committedParticipants,totalReward);
-                delete members[committedParticipants];
+            address committedParticipant = events[eventId].committedParticipants[i];
+            if(memberHasFinishedHisCommitment(committedParticipant)) {
+                uint totalReward = members[committedParticipant].currentEventRewards + members[committedParticipant].lastEventRewards;
+                token.transfer(committedParticipant,totalReward);
+                delete members[committedParticipant];
             } else {
-                members[committedParticipants].lastEventRewards += members[committedParticipants].currentEventRewards;
+                members[committedParticipant].lastEventRewards += members[committedParticipant].currentEventRewards;
             }
-            members[committedParticipants].currentEventRewards = 0;
+            members[committedParticipant].currentEventRewards = 0;
         }
         events[eventId].open = false;
         eventId++;
     }
 
+
+
     function updateEvent(uint feedback) external {
         require(events[eventId].open == true,"To give your feedback, an event must be active");
         require(feedback > 0, "feedback must be at least equal to 1");
-        if (members[msg.sender].eventsToCommit > 0 && events[eventId].feedbacks[msg.sender] == 0){
+        if (isCommittedMember() && hasNotGivenHisFeedbackYet()){
             members[msg.sender].eventsToCommit--;
             events[eventId].committedParticipants.push(msg.sender);
         }
-        if (events[eventId].feedbacks[msg.sender] == 0){
-            events[eventId].participants++;
-            events[eventId].voters.push(msg.sender);
+        if (hasNotGivenHisFeedbackYet()){
+            addMemberAsEventParticipant();
         }
 
         events[eventId].feedbacks[msg.sender] = feedback;
@@ -83,6 +85,11 @@ contract Community {
 
     }
 
+
+    function addMemberAsEventParticipant() private {
+        events[eventId].participants++;
+        events[eventId].voters.push(msg.sender);
+    }
 
     function assignReward() private view returns (uint)  {
         uint rewardFromParticipants = events[eventId].participants / events[eventId].expectedParticipants;
@@ -99,6 +106,18 @@ contract Community {
             totalFeedback += events[eventId].feedbacks[events[eventId].voters[i]];
         }
         return totalFeedback / events[eventId].participants ;
+    }
+
+    function memberHasFinishedHisCommitment(address committedMember) private view returns (bool) {
+        return members[committedMember].eventsToCommit == 0;
+    }
+
+    function isCommittedMember() private view returns (bool){
+        return members[msg.sender].eventsToCommit > 0;
+    }
+
+    function hasNotGivenHisFeedbackYet() private view returns (bool){
+        return events[eventId].feedbacks[msg.sender] == 0;
     }
 
 }
